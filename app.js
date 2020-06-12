27dd6dfe996b8e008db5960c21737a552e34e8db
root <gabasarthak20@gmail.com>
2020-06-12 08:40:58 +0000

    error remove

--- app.js
+++ app.js
@@ -16,8 +16,8 @@ var	commentRoutes 	  = require("./routes/comments"),
 	indexRoutes       = require("./routes/index");
 
 var URL = process.env.DATABASEURL || 3000;
-console.log(process.env.DATABASEURL);
-mongoose.connect("mongodb://localhost/yelp-camp", {useNewUrlParser: true, useUnifiedTopology: true, useFindAndModify: false});
+//console.log(process.env.DATABASEURL);
+mongoose.connect(URL, {useNewUrlParser: true, useUnifiedTopology: true, useFindAndModify: false});
 // mongodb://localhost/yelp-camp
 // mongoose.connect("mongodb+srv://sarthak:saru202001@cluster0-hvpay.mongodb.net/<dbname>?retryWrites=true&w=majority", {useNewUrlParser: true, useUnifiedTopology: true, useFindAndModify: false});
 
